c37837e36e4c5394a82fc92d780065cd3f67299b
Thomas Loy <ths.loy@googlemail.com>
2016-06-29 10:49:35 +0200

    Cancel more events

--- src/main/java/com/winthier/shop/listener/MarketListener.java
+++ src/main/java/com/winthier/shop/listener/MarketListener.java
@@ -2,22 +2,25 @@ package com.winthier.shop.listener;
 
 import com.winthier.shop.Market;
 import com.winthier.shop.ShopPlugin;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class MarketListener implements Listener {
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        Block block = event.getClickedBlock();
+    void onMarketEvent(Player player, Block block, Cancellable event) {
         if (block == null) return;
         if (!block.getWorld().getName().equals(ShopPlugin.getInstance().getMarket().getWorld())) return;
-        Player player = event.getPlayer();
-        if (event.getPlayer().hasPermission("shop.admin")) {
+        if (player.hasPermission("shop.admin")) {
             if (ShopPlugin.getInstance().getAdminCommand().bump(player, block)) {
                 event.setCancelled(true);
                 return;
@@ -29,5 +32,37 @@ public class MarketListener implements Listener {
             event.setCancelled(true);
             return;
         }
+    }
+
+    void onMarketEvent(Player player, Location location, Cancellable event) {
+        onMarketEvent(player, location.getBlock(), event);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        onMarketEvent(event.getPlayer(), event.getClickedBlock(), event);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+        onMarketEvent(event.getPlayer(), event.getRightClicked().getLocation(), event);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event) {
+        onMarketEvent(event.getPlayer(), event.getRightClicked().getLocation(), event);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        Player player;
+        if (event.getDamager() instanceof Player) {
+            player = (Player)event.getDamager();
+        } else if (event.getDamager() instanceof Projectile && ((Projectile)event.getDamager()).getShooter() instanceof Player) {
+            player = (Player)((Projectile)event.getDamager()).getShooter();
+        } else {
+            return;
+        }
+        onMarketEvent(player, event.getEntity().getLocation(), event);
     }
 }
