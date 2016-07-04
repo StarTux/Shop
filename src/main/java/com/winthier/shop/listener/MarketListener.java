95212fa1bb9609a36ec041dfb7ea9402c3cd4610
Thomas Loy <ths.loy@googlemail.com>
2016-07-05 01:31:37 +0200

    Allow some block interacitons

--- src/main/java/com/winthier/shop/listener/MarketListener.java
+++ src/main/java/com/winthier/shop/listener/MarketListener.java
@@ -3,6 +3,7 @@ package com.winthier.shop.listener;
 import com.winthier.shop.Market;
 import com.winthier.shop.ShopPlugin;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
@@ -11,6 +12,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
@@ -40,7 +45,47 @@ public class MarketListener implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getAction() == Action.PHYSICAL) return;
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            switch (event.getClickedBlock().getType()) {
+            case ENCHANTMENT_TABLE:
+            case ENDER_CHEST:
+            case CHEST:
+            case TRAPPED_CHEST:
+            case ACACIA_DOOR:
+            case BIRCH_DOOR:
+            case DARK_OAK_DOOR:
+            case JUNGLE_DOOR:
+            case SPRUCE_DOOR:
+            case WOOD_DOOR:
+            case WOODEN_DOOR:
+            case STONE_BUTTON:
+            case WOOD_BUTTON:
+            case ACACIA_FENCE_GATE:
+            case BIRCH_FENCE_GATE:
+            case DARK_OAK_FENCE_GATE:
+            case FENCE_GATE:
+            case JUNGLE_FENCE_GATE:
+            case SPRUCE_FENCE_GATE:
+                return;
+            }
+        }
         onMarketEvent(event.getPlayer(), event.getClickedBlock(), event);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onBlockPlace(BlockPlaceEvent event) {
+        onMarketEvent(event.getPlayer(), event.getBlock(), event);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onBlockBreak(BlockBreakEvent event) {
+        onMarketEvent(event.getPlayer(), event.getBlock(), event);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onBlockDamage(BlockDamageEvent event) {
+        onMarketEvent(event.getPlayer(), event.getBlock(), event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
