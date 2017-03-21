3292928835c91978820e3efdd31ce4d9e19a595e
StarTux <ths.loy@googlemail.com>
2017-03-21 13:13:39 +0100

    Add missing file

---
+++ src/main/java/com/winthier/shop/GenericEventsHandler.java
@@ -0,0 +1,10 @@
+package com.winthier.shop;
+
+import com.winthier.generic_events.GenericEventsPlugin;
+import org.bukkit.inventory.ItemStack;
+
+public final class GenericEventsHandler {
+    public String getItemName(ItemStack item) {
+        return GenericEventsPlugin.getInstance().getItemName(item);
+    }
+}
